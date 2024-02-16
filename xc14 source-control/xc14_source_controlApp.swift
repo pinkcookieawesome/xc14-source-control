a839d3c9c1ca3b66bb5d2ce6107675841aea8fa4
Rachel Page <rachel.chwazik@verizonwireless.com>
2024-02-16 14:02:10 -0500

    Initial Commit

---
+++ xc14 source-control/xc14_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc14_source_controlApp.swift
+//  xc14 source-control
+//
+//  Created by Rachel Page on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct xc14_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
